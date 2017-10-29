edb95b46db0339d7b0a4ccac6637bd2f67278ed2
durham-m <30428137+durham-m@users.noreply.github.com>
2017-10-28 17:51:55 -0700

    Add project files.

---
+++ Avinode/PrintMenu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Avinode
+{
+    public class PrintMenu
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Incorrect number of arguments. Usage: PrintMenu xmlFilePath pathToMatch");
+                return;
+            }
+
+            var xmlFilePath = args[0];
+            var pathToMatch = args[1];
+
+            if (!File.Exists(xmlFilePath))
+            {
+                Console.WriteLine(string.Format("Error: File {0} cannot be found.", xmlFilePath));
+                return;
+            }
+
+            var xdoc = XDocument.Load(xmlFilePath);
+            foreach (var element in xdoc.Descendants("item"))
+            {
+                var indentCount = 0;
+                var isActive = IsActive(element, pathToMatch);
+                indentCount = element.Ancestors().Count(x => x.Name == "subMenu");
+                var displayName = element.Element("displayName");
+                var path = element.Element("path");
+                Console.WriteLine(new String('\t', indentCount) + displayName.Value + ", " + path.Attribute("value").Value + (isActive ? " ACTIVE" : ""));
+            }
+
+        }
+
+        private static bool IsActive(XElement element, string pathToMatch)
+        {
+            if (element.DescendantsAndSelf().Any(x => x.Name == "path" && x.Attribute("value").Value.Equals(pathToMatch, StringComparison.OrdinalIgnoreCase)))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
